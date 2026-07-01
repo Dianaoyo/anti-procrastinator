81aa0246b4c2597b891d4e668f53497b912b3c38
Dianaoyo <diana.oyo123.com>
2026-07-01 14:42:34 +0500

    commit#3

--- lang.py
+++ lang.py
@@ -50,10 +50,6 @@ transl = {
         '5question': 'Как часто ты прокрастинируешь? Как именно ты проводишь это время?',
         '6question': 'Как бы ты хотел чтобы я с тобой общался?',
         'done with questions': 'Прекрасно! Спасибо за ответы и помни что маленькие шаги приводят к большой цели! :3',
-        # 'reminders': '''Вы можете выбрать, какие напоминания вы хотели бы получать.
-        #  Напоминания выпить воды и размяться присылаются регулярно через установленный вами промежуток времени
-        #  Напоминания поесть и лечь спать действуют по прицнипу будильника - юзер сам устанавливает время
-        #  ''',
         'dont procrastinate': 'подъем и прямиком к цели!',
         'water_on': 'Теперь я буду напоминать тебе выпить воды каждые 1.5 часа :) ',
         'water_off': 'Понял! Больше не буду напоминать тебе пить воду',
@@ -94,51 +90,3 @@ buttons = {
         'other': 'Поговорить с ии'
     }
 }
-# =======
-# transl = {
-#     'en': {
-#         'welcome': """Welcome! I'm your assistant, I'm here to help you to increase your productivity! ;)
-#     Here what I can do:
-#     • Remind you to drink water (use /water)
-#     • Remind you to sleep (use /sleep)
-#     • Remind you to eat (use /eat)
-#     • Remind you to stretch (use /stretch)
-#
-#     For the best experience you may answer a few questions.
-#     you can do it anytime by using /ready
-#     """,
-#     '1question': 'What are you trying to achieve?',
-#     '2question': 'Great goal! But why do you need it?',
-#     '3question': 'Have you done anything to achieve it ot you start with a scratch?',
-#     '4question': 'how much time are you ready to spend for it?',
-#     '5question': 'How often do you procrastinate? How exactly?',
-#     '6question': 'What character of mine are you comfortable with?',
-#     'done with questions': 'Great! Thanks for answers :3'
-#     },
-#     'ru': {
-#         'welcome': """Привет! Я твой ассистент и я помогу тебе повысить продуктивность! ;)
-#     Вот что я умею:
-#     • Напоминать пить воду (/water)
-#     • Напоминать спать (/sleep)
-#     • Напоминать есть (/eat)
-#     • Напоминать делать разминку (/stretch)
-#
-#     Для наиболее эффективной работы, ответь на парочку вопросов о себе.
-#     Чем более развернуты твои ответы, тем лучше я смогу тебе помочь!
-#     Можешь начать в любое время с помощью команды /ready
-# """,
-#         '1question': 'Какова твоя цель?',
-#         '2question': 'Отличная цель! А для чего тебе это нужно?',
-#         '3question': 'Делал ли ты уже что-то для ее достижения или начинаешь с нуля?',
-#         '4question': 'Сколько времени в день ты готов уделять?',
-#         '5question': 'Как часто ты прокрастинируешь? Как именно ты проводишь это время?',
-#         '6question': 'Как бы ты хотел чтобы я с тобой общался?',
-#         'done with questions': 'Прекрасно! Спасибо за ответы и помни что маленькие шаги приводят к большой цели! :3'
-#
-#     }
-# }
-# # transl = {
-# #     'en': {'got it': 'Got it!'},
-# #     'ru': {'got it': 'Понял!'},
-# # }
-# >>>>>>> c7dd31ba49196073f193837bfba9ec795baae08d
