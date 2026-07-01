d0b9045be871411137a48c004f26e5ea5d777656
Diana Tapakova <diana.oyo123@gmail.com>
2026-07-01 15:17:22 +0500

    Clean up main.py by removing unused code
    Removed unused imports and commented-out code related to reminders and debugging.

--- main.py
+++ main.py
@@ -56,44 +56,7 @@ async def main():
                     await water_reminder(user_id)
                     await stretch_reminder(user_id)
     scheduler.start()
-    # if debug_mode:
-        # await reminder()
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-=======
-import asyncio
-import logging
-from aiogram import Dispatcher, Bot
-import keyb as kb
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-import aiosqlite
-from handlers import user
-from datab import user_profiles_db
-
-logging.basicConfig(level=logging.INFO)
-
-dp = Dispatcher()
-
-dp.include_router(user)
-
-debug_mode = False
-
-
-# async def reminder(user_id):
-#     await bot.send_message(chat_id=user_id, text='are you going to achieve something?', reply_markup=kb.menu)
-#
-# async def main():
-#     await user_profiles_db()
-#     scheduler = AsyncIOScheduler()
-#     scheduler.add_job(reminder, 'interval', minutes=20)
-#     scheduler.start()
-#     if debug_mode:
-#         await reminder()
-
-async def main():
-    await dp.start_polling(bot)
-
-if __name__ == '__main__':
->>>>>>> c7dd31ba49196073f193837bfba9ec795baae08d
     asyncio.run(main())
