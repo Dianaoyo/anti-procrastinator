5cc7e3a9f68a018d192c2a5dcdd36324b918a52f
Diana Tapakova <diana.oyo123@gmail.com>
2026-07-01 14:48:55 +0500

    Clean up keyb.py by removing commented code
    Removed commented-out code for unused keyboard options.

--- keyb.py
+++ keyb.py
@@ -32,13 +32,6 @@ ais_style = InlineKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True
 )
 
-# make_a_break = InlineKeyboardMarkup(
-#     inline_keyboard=[
-#         [InlineKeyboardButton(text='ok', callback_data='doesnt_care')],
-#     ],
-#     resize_keyboard=True, one_time_keyboard=True
-# )
-
 options = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text='im lazy', callback_data='lazy')],
@@ -73,53 +66,3 @@ reminders_ru = InlineKeyboardMarkup(
         [InlineKeyboardButton(text='вода', callback_data='wat_ru'), InlineKeyboardButton(text='', callback_data='food_ru')],
         [InlineKeyboardButton(text='вода', callback_data='sleep_ru'), InlineKeyboardButton(text='', callback_data='')]
     ]
-=======
-from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
-                           InlineKeyboardButton, InlineKeyboardMarkup)
-
-
-mainMenu = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='start timer')],
-        [KeyboardButton(text="today's focus time")],
-    ],
-    resize_keyboard=True, one_time_keyboard=True
-)
-
-menu = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='Yes',  callback_data='work'),
-        InlineKeyboardButton(text='No',  callback_data='not_work')]
-    ],
-    resize_keyboard=True, one_time_keyboard=True
-)
-
-options = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='im lazy', callback_data='lazy')],
-        [InlineKeyboardButton(text='im tired', callback_data='tired')]
-    ],
-    resize_keyboard=True, one_time_keyboard=True
-)
-
-start_timer = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='Yes',  callback_data='start_timer'),
-        InlineKeyboardButton(text='No',  callback_data='dont_start_timer')]
-    ],
-    resize_keyboard=True, one_time_keyboard=True
-)
-
-options_timer = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='pause',  callback_data='pause_timer'),
-        InlineKeyboardButton(text='stop',  callback_data='stop_timer')]
-    ],
-    resize_keyboard=True, one_time_keyboard=True
-)
-choose_language = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text="Русский", callback_data="set_lang_ru"), InlineKeyboardButton(text="English", callback_data="set_lang_en")]
-    ]
->>>>>>> c7dd31ba49196073f193837bfba9ec795baae08d
-)
