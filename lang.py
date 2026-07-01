b9a2ca8731e537b774f1ea8aa2951be0b441a741
Diana Tapakova <diana.oyo123@gmail.com>
2026-07-01 15:15:26 +0500

    Revise assistant messages and functionality prompts
    Updated welcome message and functionality descriptions in English and Russian. Removed unnecessary comments and improved question prompts.

--- lang.py
+++ lang.py
@@ -1,42 +1,56 @@
 <<<<<<< HEAD
 transl = {
     'en': {
-        'welcome': """Welcome! I'm your assistant, I'm here to help you to increase your productivity! ;)
-    Here what I can do:
-    • Remind you to drink water (use /water)
-    • Remind you to sleep (use /sleep)
-    • Remind you to eat (use /eat)
-    • Remind you to stretch (use /stretch)
+       transl = {
+    'en': {
+        'welcome': """Hi! I'm yout assistant and i'll help you boost your productivity! ;)
+Here what i can do:
+ - remind you to drink water (/water)
+ - remind you to stretch (/stretch)
+ - track the time you've been focused (start timer)
     
-    For the best experience you may answer a few questions.
-    you can do it anytime by using /ready
-    """,
-    '1question': 'What are you trying to achieve?',
-    '2question': 'Great goal! But why do you need it?',
-    '3question': 'Have you done anything to achieve it ot you start with a scratch?',
-    '4question': 'how much time are you ready to spend for it?',
-    '5question': 'How often do you procrastinate? How exactly?',
-    '6question': 'What character of mine are you comfortable with?',
-    'done with questions': 'Great! Thanks for answers :3',
-    'water reminder': 'пора попитef',
-    'stretch reminder': 'пора бы размятьсяefвоночник!',
-    'dont procrastinate': 'подъем и прямef!',
-    'start timer': 'Таймер наef',
-    'total focus': 'сегодня ты был в фукапваусе '
+ to turn on these functions, use the commands, to turn off use them again.
+ For the most effective work, answer some questions about yourself. 
+ You can start anytime using /ready
+""",
+        '1question': 'Whats your goal?',
+        '2question': 'Great goal! But what do you need it for?',
+        '3question': 'Were you doing anything to achieve it??',
+        '4question': 'How much time are you ready to spend for it?',
+        '5question': 'How often do you procrastinate (unnecessarily delay or postpone a task)? How exactly?',
+        '6question': 'How would you like me to chat in you?',
+        'done with questions': 'Great! Thanks for answers',
+        'dont procrastinate': "Wake up! Time's not waiting!",
+        'water_on': "From now on i'll remind you to drink water every 1.5 hours :)",
+        'water_off': "Got it! i won't remind you to drink water anymore",
+        'stretch_on': "From now on i'll remind you to stretch every 1.5 hours :)",
+        'stretch_off': "Got it! i won't remind you to stretch anymore",
+        'water reminder': 'Time for water!',
+        'stretch reminder': 'Time to stretch!',
+        'start timer': "the timer's on",
+        'total focus': "you've been focused for ",
+        'no focus': "you didn't work today yet",
+        'no ai style': 'default mod set',
+        'make a break': "You've been focused for 1.5 hours, make a break!",
+        'kick of motivation': 'Need motivation',
+        'tired': 'Tired',
+        'call ai': 'Call AI',
+        'why': 'Why?',
+        'start timer?': 'Great! do i start the timer?',
+        'focus time': "you've been focused for {}"
     },
+    
     'ru': {
         'welcome': """Привет! Я твой ассистент и я помогу тебе повысить продуктивность! ;)
     Вот что я умею:
-    • Напоминать пить воду (/water)
-    • Напоминать спать (/sleep)
-    • Напоминать есть (/eat)
-    • Напоминать делать разминку (/stretch)
+    - Напоминать пить воду (/water)
+    - Напоминать делать разминку (/stretch)
+    - Мотивировать и давать советы
+    - Регулярно напоминать тебе о работе
     
 Чтобы включить эти функции, нажми на соответствующую команду
 Чтобы выключить, нажми поворно.
-    
-Также я умею засекать время (start timer), которое ты учился, и выводить статистику (today's focus time)
-    
+Также я умею засекать время (start timer), которое ты учился, и выводить время концентрации за день (today's focus time)
 Для наиболее эффективной работы, можешь ответить на парочку вопросов о себе. 
 Чем более развернуты твои ответы, тем лучше я смогу тебе помочь!
 Можешь начать в любое время с помощью команды /ready
@@ -48,10 +62,6 @@ transl = {
         '5question': 'Как часто ты прокрастинируешь? Как именно ты проводишь это время?',
         '6question': 'Как бы ты хотел чтобы я с тобой общался?',
         'done with questions': 'Прекрасно! Спасибо за ответы и помни что маленькие шаги приводят к большой цели! :3',
-        # 'reminders': '''Вы можете выбрать, какие напоминания вы хотели бы получать.
-        #  Напоминания выпить воды и размяться присылаются регулярно через установленный вами промежуток времени
-        #  Напоминания поесть и лечь спать действуют по прицнипу будильника - юзер сам устанавливает время
-        #  ''',
         'dont procrastinate': 'подъем и прямиком к цели!',
         'water_on': 'Теперь я буду напоминать тебе выпить воды каждые 1.5 часа :) ',
         'water_off': 'Понял! Больше не буду напоминать тебе пить воду',
@@ -73,64 +83,3 @@ transl = {
 
     }
 }
-
-buttons = {
-    'en': {
-        'kick of motivation': 'Motivating kick',
-        'support': 'support',
-        'other': 'talk with ai'
-    },
-    'ru': {
-        'kick of motivation': 'Пинок мотивации',
-        'support': 'Восстановление',
-        'other': 'Поговорить с ии'
-    }
-}
-=======
-transl = {
-    'en': {
-        'welcome': """Welcome! I'm your assistant, I'm here to help you to increase your productivity! ;)
-    Here what I can do:
-    • Remind you to drink water (use /water)
-    • Remind you to sleep (use /sleep)
-    • Remind you to eat (use /eat)
-    • Remind you to stretch (use /stretch)
-    
-    For the best experience you may answer a few questions.
-    you can do it anytime by using /ready
-    """,
-    '1question': 'What are you trying to achieve?',
-    '2question': 'Great goal! But why do you need it?',
-    '3question': 'Have you done anything to achieve it ot you start with a scratch?',
-    '4question': 'how much time are you ready to spend for it?',
-    '5question': 'How often do you procrastinate? How exactly?',
-    '6question': 'What character of mine are you comfortable with?',
-    'done with questions': 'Great! Thanks for answers :3'
-    },
-    'ru': {
-        'welcome': """Привет! Я твой ассистент и я помогу тебе повысить продуктивность! ;)
-    Вот что я умею:
-    • Напоминать пить воду (/water)
-    • Напоминать спать (/sleep)
-    • Напоминать есть (/eat)
-    • Напоминать делать разминку (/stretch)
-    
-    Для наиболее эффективной работы, ответь на парочку вопросов о себе. 
-    Чем более развернуты твои ответы, тем лучше я смогу тебе помочь!
-    Можешь начать в любое время с помощью команды /ready
-""",
-        '1question': 'Какова твоя цель?',
-        '2question': 'Отличная цель! А для чего тебе это нужно?',
-        '3question': 'Делал ли ты уже что-то для ее достижения или начинаешь с нуля?',
-        '4question': 'Сколько времени в день ты готов уделять?',
-        '5question': 'Как часто ты прокрастинируешь? Как именно ты проводишь это время?',
-        '6question': 'Как бы ты хотел чтобы я с тобой общался?',
-        'done with questions': 'Прекрасно! Спасибо за ответы и помни что маленькие шаги приводят к большой цели! :3'
-
-    }
-}
-# transl = {
-#     'en': {'got it': 'Got it!'},
-#     'ru': {'got it': 'Понял!'},
-# }
->>>>>>> c7dd31ba49196073f193837bfba9ec795baae08d
